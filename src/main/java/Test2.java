7729266c5d23f7fea1897e93666b54f438029cba
Creasexxx <775694251@qq.com>
2022-09-13 17:56:16 +0800

    push commit

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -9,5 +9,6 @@ public class Test2 {
       System.out.println("hello3");
       System.out.println("master test");
       System.out.println("hot-fix test");
+      System.out.println("push test");
    }
 }
